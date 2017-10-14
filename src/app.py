4b757a7137e275682e58ac6b1c928c56efb01561
Jake Howard <git@theorangeone.net>
2017-10-14 19:13:59 +0100

    redirect root to repo

--- src/app.py
+++ src/app.py
@@ -1,12 +1,18 @@
 from sanic import Sanic
 from sanic_useragent import SanicUserAgent
+from sanic import response
 import views.playlist
 from match import PLAYLIST_ID_MATCH
+
+
+def redirect_to_repo(request):
+    return response.redirect('https://github.com/realorangeone/spotify-public-proxy')
 
 app = Sanic(__name__)
 SanicUserAgent.init_app(app)
 
 app.add_route(views.playlist.get_playlist_details, '/playlist/<id:{}>'.format(PLAYLIST_ID_MATCH))
+app.add_route(redirect_to_repo, '/')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
