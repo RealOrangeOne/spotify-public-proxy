b490ef202a4e4df11ab8e2a8eb2e9063acf763ea
Jake Howard <git@theorangeone.net>
2018-01-14 18:08:11 +0000

    Update tests to mach new structure

--- tests/fixtures.py
+++
@@ -1,9 +0,0 @@
-VALID_PLAYLIST = "spotify:user:spotifycharts:playlist:37i9dQZEVXbLnolsZ8PSNw"
-INVALID_PLAYLIST_ID = "spotify:user:spotifycharts:playlist:37i9dQZEVXbLnolsZ8PSNq"
-INVALID_PLAYLIST_USER = "spotify:user:2580582903850258350:playlist:37i9dQZEVXbLnolsZ8PSNw"
-
-
-def build_playlist(user, id):
-    return "spotify:user:{}:playlist:{}".format(
-        user, id
-    )
--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,44 +1,17 @@
 from src.app import app
 from unittest import TestCase
-from tests import fixtures
-from src import match
 
+def build_playlist(user, id):
+    return "/v1/users/{}/playlists/{}".format(
+        user, id
+    )
 
 class PlaylistTestCase(TestCase):
-    def build_url(self, ident):
-        return "/playlist/{}".format(ident)
+    def test_valid_playlist(self):
+        request, response = app.test_client.get(build_playlist("spotify", "59ZbFPES4DQwEjBpWHzrtC"))
+        self.assertEqual(response.status, 200)
 
-    def test_invalid_playlist_id(self):
-        request, response = app.test_client.get(self.build_url(fixtures.INVALID_PLAYLIST_ID))
+    def test_invalid_playlist(self):
+        request, response = app.test_client.get(build_playlist("spotifyyyyy", "59ZbFPES4DQwEjBpWHzrtC"))
         self.assertEqual(response.status, 404)
-
-    def test_invalid_playlist_user(self):
-        request, response = app.test_client.get(self.build_url(fixtures.INVALID_PLAYLIST_USER))
-        self.assertEqual(response.status, 404)
-
-    def test_playlist_details(self):
-        request, response = app.test_client.get(self.build_url(fixtures.VALID_PLAYLIST))
-        self.assertEqual(response.status, 200)
-        data = response.json
-        self.assertEqual(data['name'], 'United Kingdom Top 50')
-        self.assertEqual(data['uri'], fixtures.VALID_PLAYLIST)
-        self.assertTrue(data['description'])
-        self.assertGreater(data['followers'], 0)
-
-    def test_track_data(self):
-        request, response = app.test_client.get(self.build_url(fixtures.VALID_PLAYLIST))
-        self.assertEqual(response.status, 200)
-        data = response.json['tracks'][0]
-        self.assertEqual(type(data['duration_ms']), int)
-        self.assertGreater(data['popularity'], 0)
-        self.assertRegex(data['uri'], match.TRACK_ID_MATCH)
-
-    def test_album_data(self):
-        request, response = app.test_client.get(self.build_url(fixtures.VALID_PLAYLIST))
-        self.assertEqual(response.status, 200)
-        data = response.json['tracks'][0]['album']
-        self.assertNotEqual(data['album_type'], '')
-        self.assertRegex(data['uri'], match.ALBUM_ID_MATCH)
-        for artist in response.json['tracks'][0]['album']['artists']:
-            self.assertIn(artist, response.json['tracks'][0]['artists'])
-            self.assertRegex(artist['uri'], match.ARTIST_ID_MATCH)
+        self.assertIsNone(response.json)
