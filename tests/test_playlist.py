8dc94d2e63d2d7ac1d4f78ed115b4f605691f8ba
Jake Howard <git@theorangeone.net>
2018-01-15 19:30:14 +0000

    Test querystring is passed through

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -17,3 +17,9 @@ class PlaylistTestCase(TestCase):
         request, response = app.test_client.get(build_playlist("spotifyyyyy", "59ZbFPES4DQwEjBpWHzrtC"))
         self.assertEqual(response.status, 404)
         self.assertIsNone(response.json)
+
+    def test_passes_querystring(self):
+        request, response = app.test_client.get(build_playlist("spotify", "59ZbFPES4DQwEjBpWHzrtC") + '?limit=10')
+        self.assertEqual(response.status, 200)
+        print(response.json)
+        self.assertEqual(response.json['tracks']['limit'], 10)
