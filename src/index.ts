d0041d27ec798485547f7c5d3d6d40f221205d7c
Jake Howard <git@theorangeone.net>
2019-01-20 17:49:07 +0000

    Get upstream data

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import {
 } from '@types/aws-lambda';
 
 import request from 'request-promise-native';
+import { URL } from 'url';
 
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const API_URL = "https://api.spotify.com";
 
 const CLIENT_ID_ACCESS_TOKEN = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
 
@@ -35,13 +37,26 @@ exports.handler = async (
   if (event.httpMethod != 'GET') {
     return { statusCode: 405}
   }
-  const access_token = await getAccessToken();
-  const path = getPath(event);
+  const accessToken = await getAccessToken();
+  const url = API_URL + getPath(event);
+  let response;
+  try {
+    response = await request.get(url, {
+      resolveWithFullResponse: true,
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+        "Accept": "application/json"
+      }
+    });
+  } catch (error) {
+    return {
+      statusCode: error.statusCode,
+      body: ""
+    }
+  }
   return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ path })
+    statusCode: response.statusCode,
+    body: response.body,
+    headers: response.headers
   };
 }
