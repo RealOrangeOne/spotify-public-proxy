ea0d2f3113dec9039f09718dfb0b5e06cda824ac
Jake Howard <git@theorangeone.net>
2019-01-20 15:59:55 +0000

    Rretrieve access token

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,40 @@ import {
   APIGatewayProxyEvent,
   APIGatewayProxyCallback
   // @ts-ignore
-} from '@types/aws-lambda'
+} from '@types/aws-lambda';
 
+import request from 'request-promise-native';
 
-exports.handler = (
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+
+const CLIENT_ID_ACCESS_TOKEN = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
+
+async function getAccessToken() {
+  const data = await request.post("https://accounts.spotify.com/api/token", {
+    form: {
+      grant_type: "client_credentials"
+    },
+    headers: {
+      "Authorization": `Basic ${CLIENT_ID_ACCESS_TOKEN}`,
+    }
+  });
+
+  return JSON.parse(data).access_token;
+}
+
+exports.handler = async (
   event: APIGatewayProxyEvent,
-  context: any,
-  callback: APIGatewayProxyCallback
+  context: any
 ) => {
-  callback(null, {
+  const access_token = await getAccessToken();
+  return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json'
     },
-    body: "{}"
-  });
+    body: JSON.stringify({
+      access_token
+    })
+  };
 }
